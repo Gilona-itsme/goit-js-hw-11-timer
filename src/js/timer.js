7e7ea951e1fb1d6b768e679982c0cb94a9077732
Ilona <ilona.gil@outlook.com>
2020-11-10 14:05:54 +0200

    обновляет Таймер 11/11

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,12 +1,10 @@
 
-const refs = {
-  timerClock: document.querySelector('#timer-1'),
-};
-
 export class CountdownTimer {
-  constructor({targetDate }) {
+  constructor({ selector, targetDate }) {
     this.intervalId = null;
+    this.selector = selector;
     this.targetDate = targetDate;
+   
     this.start();
   }
 
@@ -14,41 +12,11 @@ export class CountdownTimer {
     this.intervalId = setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = this.targetDate - currentTime;
-      const timeClock = this.getTime(deltaTime);
-
-      this.updateClock(timeClock);
+      this.getTime(deltaTime);
     }, 1000);
+
   }
 
-  /*
-   * - Принимает время в миллисекундах
-   * - Высчитывает сколько в них вмещается часов/минут/секунд
-   * - Рисует интерфейс
-   */
-  updateClock({ days, hours, mins, secs }) {
-    refs.timerClock.innerHTML = `<div class="field">
-    <span class="value" data-value="days">${days}</span>
-    <span class="label">Days</span>
-  </div>
-
-  <div class="field">
-    <span class="value" data-value="hours">${hours}</span>
-    <span class="label">Hours</span>
-  </div>
-
-  <div class="field">
-    <span class="value" data-value="mins">${mins}</span>
-    <span class="label">Minutes</span>
-  </div>
-
-  <div class="field">
-    <span class="value" data-value="secs">${secs}</span>
-    <span class="label">Seconds</span>
-  </div>`;
-  }
-
-  //  Высчитывает сколько в них вмещается дней/часов/минут/секунд
-  //    * - Возвращает обьект со свойствами days, hours, mins, secs
   getTime(time) {
     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
     const hours = this.pad(
@@ -56,25 +24,42 @@ export class CountdownTimer {
     );
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-
-    return { days, hours, mins, secs };
+    this.timerMarkUp({ days, hours, mins, secs });
   }
 
-  /*
-   * Принимает число, приводит к строке и добавляет в начало 0 если число меньше 2-х знаков
-   */
   pad(value) {
     return String(value).padStart(2, '0');
   }
+
+    timerMarkUp({ days, hours, mins, secs }) {
+        
+       const markUpDiv= `
+      <div class="field">
+        <span class="value" data-value="days">${days}</span>
+        <span class="label">Days</span>
+      </div>
+      <div class="field">
+        <span class="value" data-value="hours">${hours}</span>
+        <span class="label">Hours</span>
+      </div>
+      <div class="field">
+        <span class="value" data-value="mins">${mins}</span>
+        <span class="label">Minutes</span>
+      </div>
+      <div class="field">
+        <span class="value" data-value="secs">${secs}</span>
+        <span class="label">Second</span>
+      </div>`;
+       
+        document.querySelector(`#timer-1`).innerHTML = markUpDiv;
+  }
+
 }
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Dec 06, 2020'),
-  
 });
-
-timer.start();
 
 
 
